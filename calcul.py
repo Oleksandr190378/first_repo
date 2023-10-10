1f2c219e49b43ed16ff7aaea6eb8d6134b70378a
Oleksandr190378 <oleksandr190378@gmail.com>
2023-10-10 14:33:00 +0300

    commit2

--- calcul.py
+++ calcul.py
@@ -1,2 +1,26 @@
-print ('hello world')
-print ('hello John')
+
+
+
+a = int(input('Enter a: '))
+b = int(input('Enter b: '))
+oper =input('Enter operator: ')
+
+if oper == "+":
+    c = a + b
+elif oper == "-":
+    c = a-b
+elif oper == "*":
+    c = a*b
+elif oper == "/":
+    c = a/b
+else :
+    print('fncorrect operator')  
+    exit(1)
+
+
+#b =input("Enter b: ")
+
+
+print(f"result is: {c}")
+
+
