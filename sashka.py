c01c101eb638c6bdc1fe1a5a207ca42487b869ad
Oleksandr190378 <oleksandr190378@gmail.com>
2023-10-12 12:13:18 +0300

    commit4

---
+++ sashka.py
@@ -0,0 +1,10 @@
+name = "Oleks"
+
+#print("hello {} my boy".format("dima"))
+
+print(f"hello {name}, my boy. Are you ready?  ")
+a = str(input ( " "))
+if a == "yes":
+    print("Game start")
+else:
+     print("go on")         
