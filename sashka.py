c7304d26357a8255368af1a5d7bfef16ec9cfff0
Oleksandr190378 <oleksandr190378@gmail.com>
2023-10-16 13:34:47 +0300

    commit5

--- sashka.py
+++ sashka.py
@@ -1,10 +1,22 @@
-name = "Oleks"
+initial_pin ='1234'
 
-#print("hello {} my boy".format("dima"))
+n = 0
+while n < 3:
 
-print(f"hello {name}, my boy. Are you ready?  ")
-a = str(input ( " "))
-if a == "yes":
-    print("Game start")
-else:
-     print("go on")         
+    user_pin = input('enter your pin: ' )
+
+    if len(user_pin) ==4 or len(user_pin) ==6:
+        if initial_pin == user_pin:
+            print('pin is right')
+            break
+        else :
+            print('sorry. wrong pin ')
+
+    else:
+        print('pin should be 4 digits')  
+    n = n+1
+
+print('good buy')          
+
+
+
