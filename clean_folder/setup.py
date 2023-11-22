1efa3af301fde74f2e4658fccc20da46f014a3ab
Oleksandr190378 <oleksandr190378@gmail.com>
2023-11-22 14:05:00 +0200

    changea6

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -8,5 +8,7 @@ setup(
     author_email='oleksandr190378@gmail.com',
     license='MIT',
     packages=find_namespace_packages(),
-    entry_points={"console_script": [ "clean_folder = clean_folder.clean:main"]} 
+    entry_points={'console_scripts': [ 'clean_folder = clean_folder.clean:main']} 
            )              
+
+
