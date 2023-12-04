bae9374c1b5559d533e0564cc2258ac951f065b5
Oleksandr190378 <oleksandr190378@gmail.com>
2023-12-04 17:51:46 +0200

    changes11

--- home9.py
+++ home9.py
@@ -4,51 +4,73 @@ phone_dic = {}
 
 def inner():
     st = input('Enter the command: ')
-    res = st.split()
+    res = st.split()+ [' '] + [' ']
     return res
+
+def decorator(func):
+    def wrapper_decorator(el):
+        if el.isalpha():
+            return func(el)
+        else:
+            print('TypeError')        
+    return wrapper_decorator   
+
+@decorator    
+def phone(el:str):
+    if el in phone_dic:
+        print(f'the {el} has a {phone_dic[el]} number')
+    else:
+        print('The contact is not in the dictionary')      
 
 def hello():
     print('How can I help you?')
 
+def Error_Handler(func):
+    def Inner_Function(el1, el2):
+        if el1.isalpha() and el2.isdecimal():
+            print('Done')
+            return func(el1,el2)
+        else:
+            print(" wrong data types")
+    return Inner_Function
+
+@Error_Handler
 def add(el1, el2):
     phone_dic.update({el1:el2})   
-    print('Added')
-
+@Error_Handler
 def change(el1, el2):
     if el1 in phone_dic:
         phone_dic[el1] = el2
-        print('Changed')
     else:
         print('The contact is not the dictionary')    
-    
-def phone(el):
-    if el in phone_dic:
-        print(f'the {el} has a {phone_dic[el]} number')
-    else:
-        print('The contact is not the dictionary')      
 
 def show():
     print(phone_dic) 
 
 dic_func = {'hello': hello, 'add': add, 'change': change, 'phone': phone, 'show': show,
-             'good_bye': 0, 'exit': 0, 'false': 0}
+             'good bye': 0, 'exit': 0, 'close': 0}
 
 def entry(arg):
         e = dic_func[arg]
         return e
 
+def good_bye(x:str, y:str):
+    return x + ' ' + y
+
 def do_work():
     args = sys.argv
-    args = args[1:] # First element of args is the file name
+    args = args[1:] + [' '] + [' ']# First element of args is the file name
     ind = True
     while ind:
-        if len(args) == 0:
+        if len(args) == 2:
             print('You have not passed any commands in!')
             args = inner()
         else:
             g = True
             for a in range(len(args)):
                 args[a] = args[a].lower()
+                if args[a] == 'good':
+                    args[a] = good_bye(args[a], args[a+1])
                 if args[a] in dic_func:
                     g = False
                     if args[a] in ['hello', 'show']:
@@ -56,25 +78,16 @@ def do_work():
                         y()
                         args = inner()
                         break
-                    elif args[a] in ['good_bye', 'exit', 'false']:
+                    elif args[a] in ['good bye', 'exit','close']:
                         ind = False        
                     elif args[a] == 'phone':
-                        try:
-                            y = entry(args[a])    
-                            y(args[a+1])
-                        except:
-                            print('ValueError')
+                        y = entry(args[a])    
+                        y(args[a+1])
                         args = inner() 
                         break   
                     elif args[a] in ['add', 'change']:
-                        try:
-                            if args[a+1].isalpha() and args[a+2].isdecimal():
-                                y = entry(args[a])
-                                y(args[a+1], args[a+2])   
-                            else:
-                                print('KeyError')    
-                        except:
-                            print('ValueError or KeyError')
+                        y = entry(args[a])
+                        y(args[a+1], args[a+2])     
                         args = inner()
                         break   
             if g:    
