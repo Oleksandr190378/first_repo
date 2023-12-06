b96df00f5beefe9a148665ab9371c77cd07c3c55
Oleksandr190378 <oleksandr190378@gmail.com>
2023-12-06 15:11:22 +0200

    changes14

--- home9.py
+++ home9.py
@@ -7,42 +7,38 @@ def inner():
     res = st.split() + [' '] + [' ']
     return res
 
-def hello():
-    print('How can I help you?')
-
 def Input_Error(func):
     def Inner_Function(*el):
-        try:  
+        try:
+            el.index(' ') 
+            return'Enter data after the command'
+        except:
             try:
                 return func(*el)
             except ValueError:
-                print('Please enter a valid name or number!') 
+                return 'Please enter a valid name or number!' 
             except TypeError: 
-                print('The contact  is not in the dictionary') 
-        except IndexError:     
-            print('Enter data after the command')              
+                return 'The contact  is not in the dictionary'                       
     return Inner_Function
 
 @Input_Error
-def add(el1, el2):
-    if el1 == ' ' or el2 == ' ':
-        raise IndexError
+def add(el1:str, el2:str):
     if el1.isalpha():
         if el2.isnumeric():
             phone_dic.update({el1:el2})
+            return "Done"
         else:    
             raise ValueError
     else:
         raise ValueError       
 
 @Input_Error
-def change(el1, el2):
-    if el1 == ' ' or el2 == ' ':
-        raise IndexError
+def change(el1:str, el2:str):
     if el1.isalpha():
         if el2.isnumeric():
             if el1 in phone_dic:
-                phone_dic[el1] = el2 
+                phone_dic[el1] = el2
+                return "Done" 
             else:
                 raise TypeError   
         else:    
@@ -52,20 +48,18 @@ def change(el1, el2):
 
 @Input_Error
 def phone(el:str):
-    if el == ' ':
-        raise IndexError
     if el.isalpha():
         if el in phone_dic:
-            print(f'the {el} has a {phone_dic[el]} number')
+            return phone_dic[el]
         else:
             raise TypeError   
     else:
         raise ValueError
     
 def show():
-    print(phone_dic) 
+    return phone_dic
 
-dic_func = {'hello': hello, 'add': add, 'change': change, 'phone': phone, 'show': show,
+dic_func = {'add': add, 'change': change, 'phone': phone, 'show': show,
              'good bye': 0, 'exit': 0, 'close': 0}
 
 def entry(arg):
@@ -87,28 +81,32 @@ def do_work():
             g = True
             for a in range(len(args)):
                 args[a] = args[a].lower()
+                if args[a] == 'hello':
+                    print('How can I help you?')
+                    args = inner()
+                    g = False
+                    break
                 if args[a] == 'good':
                     args[a] = good_bye(args[a], args[a+1])
                 if args[a] in dic_func:
                     g = False
-                    if args[a] in ['hello', 'show']:
+                    if args[a] == 'show':
                         y = entry(args[a])
-                        y()
+                        print(y())
                         args = inner()
                         break
                     elif args[a] in ['good bye', 'exit','close']:
                         ind = False        
                     elif args[a] == 'phone':
                         y = entry(args[a])    
-                        y(args[a+1])
+                        print(y(args[a+1]))
                         args = inner() 
                         break   
                     elif args[a] in ['add', 'change']:
-                        y = entry(args[a])
-                        y(args[a+1], args[a+2])     
+                        y = entry(args[a])   
+                        print(y(args[a+1], args[a+2]))
                         args = inner()
-                        break
-                        
+                        break                     
             if g:    
                 print('Unrecognized command')
                 args = inner()
