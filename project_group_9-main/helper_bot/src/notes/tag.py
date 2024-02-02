17663320f48503bbf8501d51cb56499ac31996cf
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-02-02 18:49:28 +0200

    Delete project_group_9-main directory

--- project_group_9-main/helper_bot/src/notes/tag.py
+++
@@ -1,9 +0,0 @@
-from helper_bot.src.notes.field import Field
-
-
-class Tag(Field):
-    def validate(self, value):
-        return value
-
-    def check_availability(self, value: str):
-        return value in self.value
