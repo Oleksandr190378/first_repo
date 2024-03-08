b15090dd097ef68816dfc9c0efdeb48c9e84de27
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-03-08 10:27:36 +0200

    Delete query_10.sql

--- query_10.sql
+++
@@ -1,6 +0,0 @@
-select distinct s2.fullname as student_name, g2.fullname as teacher_name, g2.sub as subject_name
-from (select g.student_id, s1.fullname, s1.sub from (select t.fullname, s.id, s.name as sub from teachers t join subjects s on s.teacher_id = t.id where t.id = 1) as s1 
-join grades g on s1.id = g.subject_id) as g2
-join students s2 on g2.student_id = s2.id where s2.id = 20; 
-
---пошук по id тудента 2.id = 20 та id викладача t.id = 1
