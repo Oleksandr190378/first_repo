b9c0a9dbda981c82795a3cd6766c3736d937b333
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-03-08 10:27:00 +0200

    Delete query_9.sql

--- query_9.sql
+++
@@ -1,5 +0,0 @@
-select distinct  w.fullname, s1.name as subject_name
-from (select s.fullname, g.subject_id from students s join grades g on g.student_id =s.id where s.id = 20) as w
-right join subjects s1 on w.subject_id = s1.id; 
-
---пошук по id студента s.id = 20
