7446e2ab6a14ade065f1d984c005d1672c891086
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-03-08 10:26:23 +0200

    Delete query_8.sql

--- query_8.sql
+++
@@ -1,4 +0,0 @@
-select  ROUND(AVG(g.grade), 2) AS average_grade_subject
-from (select  s.id  from subjects s join teachers t on s.teacher_id = t.id where t.id = 2 ) as s1
-join grades g on g.subject_id = s1.id;
---t.id = 2  id вчителя
