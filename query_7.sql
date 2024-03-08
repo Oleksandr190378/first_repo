5f6e9800979fb54a378e18b68448fadeb90e2a7a
Oleksandr190378 <147439788+Oleksandr190378@users.noreply.github.com>
2024-03-08 10:26:08 +0200

    Delete query_7.sql

--- query_7.sql
+++
@@ -1,6 +0,0 @@
-select  s1.fullname, g.grade, s1.name as group_name, g.name as subject
-from (select s.id, s.fullname, g1.name from students s join "groups" g1 on s.group_id  = g1.id  where g1.id = 2) as s1
-join (select * from grades g2 join subjects s2 on g2.subject_id = s2.id where s2.id = 1) as g on g.student_id = s1.id;
-
---g1.id = 2 - id групи
---і2id = 1 - id редмету
